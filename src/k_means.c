38cd5c22c80f1c8a47594e10d54b2281a9c0e97c
I-Mic <novaismanel@gmail.com>
2022-10-15 16:10:35 +0100

    Ponto 1 feito

--- src/k_means.c
+++ src/k_means.c
@@ -13,6 +13,7 @@ typedef struct point{
 struct point points[N];
 struct point cluster[K];
 
+//Cria os pontos aleatorios e os clusters
 void inicializa() {
 
 	srand(10);
@@ -31,18 +32,40 @@ void inicializa() {
 	}
 }
 
-float distancia(point a, point b){
+//Funcao que calcula a distancia euclidiana entre dois pontos
+float distancia_euclidiana(point a, point b){
 	return sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
 }
 
-void atribuir_cluster(){
 
+//Funcao que atribui aos pontos o seu cluster mais proximo
+void atribuir_cluster(){
+	for (int i=0; i < N; i++){
+		int cluster_mais_proximo = 0;
+
+		//possivel melhoria de performance aqui, usando uma variavel point enves
+		//de ir sempre ao array buscar o ponto.
+		
+		float menor_distancia = distancia_euclidiana(points[i],cluster[0]);
+		for (int j = 1;j < K; j++){
+			float distancia = distancia_euclidiana(points[i],cluster[j]);
+			if(distancia < menor_distancia){
+				menor_distancia = distancia;
+				cluster_mais_proximo = j;
+			}
+		}
+		points[i].cluster_atribuido = cluster_mais_proximo;
+	}
 }
 
 
 void main(){
 	inicializa();
-	for(int i = 0; i < N; i++) {
-		printf("(%f,%f)",points[i].x,points[i].y);
+	atribuir_cluster();
+	for (int i=0;i<K;i++){
+		printf("Cluester = (%f , %f)\n",cluster[i].x,cluster[i].y);
+	}
+	for(int i = 0; i < 100; i++) {
+		printf("Ponto (%f , %f)-Cluster atribuido = %d\n",points[i].x,points[i].y,points[i].cluster_atribuido);
 	}
 }
