2bc0cceb7c83e57a814072a82b9dad9e31423027
joanaaVO <joanaveigaoliveira@gmail.com>
2022-11-06 23:00:07 +0000

    algumas alterações

--- Fase2/src/k_means.c
+++ Fase2/src/k_means.c
@@ -42,16 +42,16 @@ float distancia_euclidiana(point a, point b){
 
 //Compares the old centroid with the new one to check if it has changed
 //If it has changed returns 0 otherwise returns 1
-int comparar_centroides(int K){
-	for(int i = 0; i < K; i++){
+int comparar_centroides(int k){
+	for(int i = 0; i < k; i++){
 		if(clusters[i].centroide.x != centroides_antigos[i].x || clusters[i].centroide.y != centroides_antigos[i].y) return 0;
 	}
 	return 1;
 }
 
 //Resets the clusters, saving the current centroids, for the next iteration
-void reset_clusters(int K){
-	for (int i = 0; i < K; i++){
+void reset_clusters(int k){
+	for (int i = 0; i < k; i++){
 		centroides_antigos[i] = clusters[i].centroide; 
 		clusters[i].used = 0;
 	}
@@ -64,18 +64,19 @@ void adicionar_ponto_cluster(int k, point p) {
 
 //Function that designates a point to the closest cluster
 //If any cluster changed at the end returns 1, otherwise returns 0.
-int atribuir_clusters(int N, int K, int T) {
+int atribuir_clusters(int n, int k, int t) {
 	int cluster_mais_proximo;
 	float menor_distancia, distancia;
 	point cent = clusters[0].centroide, p;
 
-	for (int i = 0; i < N; i++){
+	//#pragma omp parallel num_threads(T)
+	for (int i = 0; i < n; i++){
 		p = points[i];
 		cluster_mais_proximo = 0;
 
 		menor_distancia = distancia_euclidiana(p,cent);
 
-		for (int j = 1; j < K; j++){
+		for (int j = 1; j < k; j++){
 			
 			distancia = distancia_euclidiana(p,clusters[j].centroide);
 
@@ -91,51 +92,66 @@ int atribuir_clusters(int N, int K, int T) {
 		}
 		adicionar_ponto_cluster(cluster_mais_proximo,p);
 	}
-	for (int k = 0; k < K; k++){
-		clusters[k].centroide = calcular_centroide(k);
+	for (int i = 0; i < k; i++){
+		clusters[i].centroide = calcular_centroide(i);
 	}
-	return comparar_centroides(K);
+	return comparar_centroides(k);
 }
 
 //Creates N Random points and assigns the first K points as centroids of each cluster
-void inicializa(int N, int K) {
+void inicializa(int n, int k) {
+	points = (struct point*) malloc(n * sizeof(struct point));
+	clusters = (struct cluster*) malloc(k * sizeof(struct cluster));
+	centroides_antigos = (struct point*) malloc(k * sizeof(struct point));
+
 
 	srand(10);
-	for(int i = 0; i < N; i++) {
+	for(int i = 0; i < n; i++) {
 		points[i].x = (float) rand() / RAND_MAX;
 		points[i].y = (float) rand() / RAND_MAX;
 		}
 
-	for(int i = 0; i < K; i++) {
+	for(int i = 0; i < k; i++) {
 		clusters[i].centroide.x = points[i].x;
 		clusters[i].centroide.y = points[i].y;
 		clusters[i].used = 0;
-		clusters[i].points = (struct point*) malloc(N * sizeof(struct point));
+		clusters[i].points = (struct point*) malloc(n * sizeof(struct point));
 	}
 }
 
 
 //Function that aplies Lloyd algorithm
 //Only ends when the application has converged 
-void k_means_lloyd_algorithm(int N, int K, int T) {
+void k_means_lloyd_algorithm(int n, int k, int t) {
 	int iteracoes = 0;
 
-	inicializa(N,K);
+	inicializa(n,k);
 
-	while(!atribuir_clusters(N,K,T)) {
-		reset_clusters(K);
+	while(!atribuir_clusters(n,k,t)) {
+		reset_clusters(k);
 		iteracoes++;
 	}
 
-	printf("N = %d, K = %d\n",N,K);
+	printf("N = %d, K = %d\n",n,k);
 
-	for(int k = 0; k < K; k++){
-		printf("Center: (%0.3f, %0.3f) : Size: %d\n",clusters[k].centroide.x,clusters[k].centroide.y,clusters[k].used);
+	for(int i = 0; i < k; i++){
+		printf("Center: (%0.3f, %0.3f) : Size: %d\n",clusters[i].centroide.x,clusters[i].centroide.y,clusters[i].used);
 	}
 	printf("Iterations:%d\n",iteracoes);
 }
 
 int main(int argc, char *argv[]){
-	k_means_lloyd_algorithm(atoi(argv[0]), atoi(argv[1]), atoi(argv[2]));
+	int n = atoi(argv[2]);
+	int k = atoi(argv[3]);
+
+	//RUN: make -> make runseq CP_CLUSTERS=4
+	if (argc == 4) {
+		k_means_lloyd_algorithm(n,k,1);
+	}
+	if (argc == 5) {
+		int t = atoi(argv[4]);
+		k_means_lloyd_algorithm(n,k,t);
+	}
+	
 	return 0;
 }
